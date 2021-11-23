788b28fc54c762f324c7a0f00f3416839dc1441d
hwanginkyeong <inke0831@naver.com>
2021-11-23 09:51:04 +0900

    1st

---
+++ src/com/javaex/ex02/Point.java
@@ -0,0 +1,35 @@
+package com.javaex.ex02;
+
+public class Point {
+	
+	//필드
+		private int x;
+		private int y;
+		
+		//메소드
+		public void setX(int x) {
+			
+		}
+		
+		public int getX() {
+			return x;
+		}
+		
+		
+		public void setY(int y) {
+			
+		}
+		public int get() {
+			return y;
+		}
+		
+		public void draw() {
+			//System.out.println("점" + x,y + "] 을 그렸습니다");
+			//System.out.println("점" + x,y + "]을 그렸습니다");
+		}
+		
+
+	
+
+
+}
---
+++ src/com/javaex/ex02/PointApp.java
@@ -0,0 +1,13 @@
+package com.javaex.ex02;
+
+public class PointApp {
+
+	public static void main(String[] args) {
+		
+		//Goods int x = new Goods();
+		//x.setX("5")
+		
+
+	}
+
+}
