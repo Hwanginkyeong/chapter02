767ab63234ed5c9c0159d4bda818d2e2b57776e6
hwanginkyeong <inke0831@naver.com>
2021-11-24 12:12:31 +0900

    1st

--- src/com/javaex/ex05/Song.java
+++ src/com/javaex/ex05/Song.java
@@ -3,10 +3,6 @@ package com.javaex.ex05;
 public class Song {
 	
 	//필드
-	
-	
-	
-	//생성자
 	private String title;
 	private String artist;
 	private String album;
@@ -14,12 +10,26 @@ public class Song {
 	private String year;
 	private int track;
 	
-	
-	//메소드 - g/s
+	//생성자 아래 둘 중에 하나만 선택해서 하거나 필요없으면 지워짐 
 	public Song() {
-		
+		//메모리에 올리는 일
+		System.out.println("Song(0)");
 	}
 	
+	
+	public Song(String title, String artist, 
+			String album, String composer, String year, int track) {
+		//메모리에 올리는 일
+		this.title = title;
+		this.artist = artist;
+		this.album = album;
+		this.composer = composer;
+		this.year = year;
+		this.track = track;
+		System.out.println("song(6)");
+	}
+		
+	//메소드 - g/s	
 	public String getTitle() {
 		return title;
 	}
@@ -64,6 +74,12 @@ public class Song {
 	public String toString() {
 		return "Song [title=" + title + ", artist=" +artist + ", album=" + album +", year="+ year +", track=" 
 	+ track + "]";
+	}
+	
+	public void showInfo() {
+		System.out.println(title+", "+artist+"("+album+","+composer+year+track);
+		
+	
 	}
 
 }
--- src/com/javaex/ex05/SongApp.java
+++ src/com/javaex/ex05/SongApp.java
@@ -3,8 +3,25 @@ package com.javaex.ex05;
 public class SongApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Song s01 = new Song();
+		s01.setTitle("좋은날");
+		s01.setArtist("아이유");
+		s01.setAlbum("Real");
+		s01.setComposer("이민수");
+		s01.setYear("2010");
+		s01.setTrack(3);
+		
+		//System.out.println(s01.toString());
+		
+		Song s02 = new Song("거짓말", "BIGBANG", "Always", "G-DRAGON", "2000", 3 );
+		//System.out.println(s02.toString());
+		
+		
+		//아이유출력
+		s01.showInfo(); //얘가 프린트엘엔까지 가지고 있음
+		//빅뱅출력
+		
 	}
 
 }
