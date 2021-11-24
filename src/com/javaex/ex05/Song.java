1247cf52ef841d69e8ab68e92550855b144315ea
hwanginkyeong <inke0831@naver.com>
2021-11-24 10:27:31 +0900

    1st

--- src/com/javaex/ex05/Song.java
+++ src/com/javaex/ex05/Song.java
@@ -1,5 +1,69 @@
 package com.javaex.ex05;
 
 public class Song {
+	
+	//필드
+	
+	
+	
+	//생성자
+	private String title;
+	private String artist;
+	private String album;
+	private String composer;
+	private String year;
+	private int track;
+	
+	
+	//메소드 - g/s
+	public Song() {
+		
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getArtist() {
+		return artist;
+	}
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+	public String getAlbum() {
+		return album;
+	}
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+	public String getComposer() {
+		return composer;
+	}
+	public void setComposer(String composer) {
+		this.composer = composer;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	public int getTrack() {
+		return track;
+	}
+	public void setTrack(int track) {
+		this.track = track;
+	}
+	
+	
+	
+	//메소드 - 일반
+	@Override
+	public String toString() {
+		return "Song [title=" + title + ", artist=" +artist + ", album=" + album +", year="+ year +", track=" 
+	+ track + "]";
+	}
 
 }
