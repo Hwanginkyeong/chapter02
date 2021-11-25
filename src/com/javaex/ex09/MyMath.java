50a413d90acd2bcc056d2741eb03016d99e21750
hwanginkyeong <inke0831@naver.com>
2021-11-25 14:23:21 +0900

    1st

--- src/com/javaex/ex09/MyMath.java
+++ src/com/javaex/ex09/MyMath.java
@@ -9,23 +9,28 @@ public class MyMath {
 	//메소드 g/s  -- 필드가 없기 때문에 없음
 	
 	//메소드 일반
-	public int plus(int a,int b) { //plus로 통칭해도 됨. 이유는 파라미터가 이미 다르기 때문.
+	public static int plus(int a,int b) { //plus로 통칭해도 됨. 이유는 파라미터가 이미 다르기 때문.
 									//이게 메소드 오버로딩. 
 		return a+b;
 	}
 	
-	public double plus(double a,double b ) {
+	public static double plus(double a,double b ) {
 		return a+b;
 	}
-	public double plus(double a, int b) {
+	public static double plus(double a, int b) {
 		//double sum = a+b;    //3.5 + 3.0 으로 바뀐다. 자동형변환
 		return a+b;
 	}
 	
-	public double plus(int a, double b) {
+	public static double plus(int a, double b) {
 		return a+b;
 	}
-	
-	
+	public static double circleArea(int radius) {
+		double area = radius*radius*3.14;
+		return area;
+	}
+	public static double circleRound(int radius) {
+		return (radius+radius)*3.14;
+	}
 
 }
--- src/com/javaex/ex09/MyMathApp.java
+++ src/com/javaex/ex09/MyMathApp.java
@@ -16,6 +16,23 @@ public class MyMathApp {
 		System.out.println(sum01);
 		//System.out.println(myMath.plusInt(3, 6));
 		
+		double sum02 = MyMath.plus(4.3, 5.5);
+		System.out.println(sum02);
+		
+		double sum03 = MyMath.plus(5.5, 9);
+		System.out.println(sum03);
+		
+		double sum04 = MyMath.plus(20, 99.9);
+		System.out.println(sum04);
+		
+		double sum05 = MyMath.circleArea(5);
+		System.out.println(sum05);
+		
+		double sum06 = MyMath.circleRound(5);
+		System.out.println(sum06);
+		
+		
+		/*
 		double sum02 = myMath.plus(4.3, 5.5);
 		System.out.println(sum02);
 		
@@ -24,7 +41,7 @@ public class MyMathApp {
 		
 		double sum04 = myMath.plus(20,99.9);
 		System.out.println(sum04);
-		
+		*/
 
 	}
 
